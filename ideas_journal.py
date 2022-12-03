295c3f4ca456f831fdedf6b1e4c2b817a12fe876
Chris Lipinski <gooinski@gmail.com>
2022-12-03 22:40:41 +1100

    css lol

---
+++ ideas_journal.py
@@ -0,0 +1,11 @@
+# Ideas
+
+# Into the vault: reflections from past work journals
+# 2013 shift to startup
+# 2014 uber growth (melbourne perspective)
+# 2017 five years of inspire9 (organic vs monocolutre)
+# 2019 3 years too long (staying in a role for too long)
+# 2020 My year of bookselling
+# 2021 niche retailing
+# 2022 learning to create product
+
